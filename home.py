86ccd957686d5a5d6855575feadc634d2329e37f
duhyun2501 <duhyun2501@gmail.com>
2026-05-22 11:27:45 +0900

    페이지 섹션 나누기

---
+++ home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.write("홈")
+
+name = st.text_input("이름을 입력하세요", key='user_name')
+if st.session_state.get('user_name'):
+    st.success(f"반갑습니다 {st.session_state['user_name']}님")
